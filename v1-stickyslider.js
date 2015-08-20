bba7aed432e92a153e122c9c710fd1dad739fa30
Alexa Schreffler <alexa.schreffler@gmail.com>
2015-08-19 22:25:38 -0500

    added slide out effect on slider close

--- v1-stickyslider.js
+++ v1-stickyslider.js
@@ -157,8 +157,8 @@ jQuery(document).ready(function() {
 		});
 
 		jQuery('.xlink, .btn').click(function() {
+			alert('{closed-modal|view-cart|checkout}');
 			jQuery('#stickyDiv').animate({"right":'-600px'}, "slow");
-			//alert('{closed-modal|view-cart|checkout}');
 		});
 	}
 });
